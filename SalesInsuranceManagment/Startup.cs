ddc5974b34a9380401a23fea2e68d1351b5b0231
DDimitrov92 <D_Dimitrov92@outlook.com>
2017-04-05 20:43:11 +0300

    Create MVC Project
    Create tables without properties

---
+++ SalesInsuranceManagment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesInsuranceManagment.Startup))]
+namespace SalesInsuranceManagment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
